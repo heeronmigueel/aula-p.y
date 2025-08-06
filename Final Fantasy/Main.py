462beff2e5cdf6ac3fd9f132343de120dd530239
heeronmigueel <heronmiguel44@gmail.com>
2025-08-06 18:13:53 +0000

    Tabuada

---
+++ Final Fantasy/Main.py
@@ -0,0 +1,12 @@
+def main():
+    inicio = int( input("Digite o inicio do intervalo"))
+    fim = int(input ("Digite o fim do intervalo"))
+intervalo = ""
+i = 1
+j = 20
+while i < j:
+
+
+
+    return 0
+main()
